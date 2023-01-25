041d31db44ada1a54092741c22e3a23392ae5870
hkan <hermann.kan.dev@gmail.com>
2023-01-25 13:24:54 -0500

    init

---
+++ .eslintrc.js
@@ -0,0 +1,86 @@
+// {
+//   "root": true,
+//   "parser": "@typescript-eslint/parser",
+//   "parserOptions": {
+//     "project": "tsconfig.json",
+//     "sourceType": "module"
+//   },
+//   "env": {
+//     "node": true,
+//     "jest": true
+//   },
+//   "plugins": ["@typescript-eslint"],
+//   "extends": [
+//     "plugin:@typescript-eslint/eslint-recommended",
+//     "plugin:@typescript-eslint/recommended"
+//   ],
+//   "rules": {
+//     "quotes": ["error", "single"],
+//     "indent": [
+//       "error",
+//       2,
+//       {
+//         "MemberExpression": 1
+//       }
+//     ],
+//     "@typescript-eslint/typedef": [
+//       "error",
+//       {
+//         "memberVariableDeclaration": true,
+//         "propertyDeclaration": true,
+//         "variableDeclaration": true,
+//         "variableDeclarationIgnoreFunction": true,
+//         "arrowParameter": false
+//       }
+//     ],
+//     "@typescript-eslint/no-empty-interface": "off",
+//     "@typescript-eslint/no-inferrable-types": "off",
+//     "@typescript-eslint/semi": ["error", "always"]
+//   }
+// }
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    quotes: ['error', 'single'],
+    indent: [
+      'error',
+      2,
+      {
+        MemberExpression: 1,
+      },
+    ],
+    '@typescript-eslint/typedef': [
+      'error',
+      {
+        memberVariableDeclaration: true,
+        propertyDeclaration: true,
+        // variableDeclaration: true,
+        variableDeclarationIgnoreFunction: true,
+        arrowParameter: false,
+      },
+    ],
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/semi': ['error', 'always'],
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
---
+++ README.md
@@ -0,0 +1,78 @@
+# Typescript backend
+
+It is my attempt to build REST API Using Typescript & NestJS.
+
+## Table of contents
+
+- [Overview](#CarDeal)
+- [Resources](#Resources)
+- [Local Developement](#Local_Developement)
+- [Demo](#CarDeal)
+- [Built with](#built-with)
+- [Author](#author)
+
+## CarDeal
+
+CardDeal is a simple fictional application that allows users to get an instant online car valuation. In other words, carDeal will allow users to get an estimate of what they car is worth if they planning on buying or selling a car.
+
+## Resources
+
+1. User - to manage authentication and CRUD actions around a user
+2. Report - to manage CRUD actions around the valuation report
+
+## Local_Developement
+
+- **Docker**
+
+  All necessary external services are described in the [./docker-compose.yml](./docker-compose.yaml):
+
+  - Run `docker-compose -f docker-compose.yaml up`
+  - Stop `docker-compose -f docker-compose.yaml stop`
+
+  Services:
+
+  1. PostgreSQL - [Credentials](.env).
+  1. Redis - [Credentials](.env).
+
+* **Building**
+
+  1. Install libraries - `npm install`
+  2. Build application - `npm run build`
+
+* Start application - `npm run start`
+
+  - Expose [./env/local.app.env](./env/local.app.env) and start application - `npm run start:local`
+
+    <details>
+      <summary>
+        API documentation will be available on the endpoint <i>GET <a href="http://localhost:3000/api/" target="_blank" rel="noopener noreferrer">http://localhost:3000/api</a></i>
+      </summary>
+      <br>
+      
+      <p align="center"> 
+          <img src="./asset/ApiDocumentation.png">
+      </p>
+    </details>
+
+* **Testing**
+
+  - Prepare environment - `docker-compose -f docker-compose.test.yaml up -d`
+  - Run tests - `npm run test`
+  - Run tests with coverage - `npm run test:cov`
+
+## Demo
+
+A client is available at:
+
+- Live Site URL: [CarDeal Demo](https://invoicev1.herokuapp.com/)
+
+## Built with
+
+- [Typescript](https://www.typescriptlang.org/docs/) - JavaScript with syntax for types.
+- [Nestjs](https://expressjs.com/) - Framework for Node.js that fully supports TypeScript
+- [PostgreSQL](https://www.postgresql.org/) - Database
+- [Redis](https://www.postgresql.org/) - Session storage
+
+## Author
+
+- Website - [Hermann](https://hkfolio.com/)
