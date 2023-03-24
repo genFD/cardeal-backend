06feaf9de82c643c58fb9b90ca13099a714da6d2
hkan <hermann.kan.dev@gmail.com>
2023-03-24 17:03:39 -0400

    s3 setup fixbug

--- terraform/modules/web-app/s3.tf
+++ terraform/modules/web-app/s3.tf
@@ -1,5 +1,12 @@
 resource "aws_s3_bucket" "app_public_files" {
   bucket_prefix = "${var.prefix}-files"
-  acl           = "public-read"
   force_destroy = true
+  tags = {
+    Name        = "${var.prefix}-public-bucket"
+  }
+}
+
+resource "aws_s3_bucket_acl" "s3_acl" {
+  bucket = aws_s3_bucket.app_public_files.id
+  acl    = "public-read"
 }
